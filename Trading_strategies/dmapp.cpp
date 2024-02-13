a7c2e7ee9d001971e6b167e91b59fea8525a83fe
Shivam Sawarn <137722393+shivamsawarn@users.noreply.github.com>
2024-02-13 18:05:57 +0530

    Update dmapp.cpp

--- Trading_strategies/dmapp.cpp
+++ Trading_strategies/dmapp.cpp
@@ -89,14 +89,14 @@ double calc_AMA_dmapp(double AMA_prev, double price_today, double SF_today, int 
 
 double percent_difference_dmapp(double AMA, double price_today)
 {
-    return 100.0 * (AMA - price_today) / AMA;
+    return 100.0 * (price_today - AMA) / AMA;
 }
 
 double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>> &CashFlow, vector<vector<string>> &order_stats, int n, int x, double p, int max_hold_days, double c1, double c2)
 {
     order_stats.push_back({"Date", "Order_dir", "Quantity", "Price"});
-    // CashFlow.push_back({"Date", "CashFlow", "AMA", "Price", "SF", "ER", "percent diff", ""});
-    CashFlow.push_back({"Date", "CashFlow"});
+    CashFlow.push_back({"Date", "CashFlow", "AMA", "Price", "SF", "ER", "percent diff", ""});
+    // CashFlow.push_back({"Date", "CashFlow"});
 
     int curr_pos = 0;
     double cumulative_cashflow = 0;
@@ -107,9 +107,10 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
     for (int i = 0; i < x; i++)
     {
         arr2[i] = -1;
-    }                       // this contains the positions
+    } // this contains the positions
+
     int first_buy_sell = 0; // stores the index of first share I bought/sell
-    int this_buy_sell = 0;
+    int this_buy_sell = 0;  // stores where next buy.sell should position at
     double arr[n];
     for (int i = 0; i < n; i++)
     {
@@ -118,22 +119,19 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
 
     double price_today = stod(data[n - 1][1]);
     double ER = 0;
-    double SF_today = 0;
-    double AMA_today = 0;
+    double SF_today = 0.5;
+    double AMA_today = stod(data[n][1]);
 
+    int curr_pos_at_start = 0;
     for (int i = n; i < data_length; i++)
     {
-        // cout << (i - 1) % n << ":::   ";
+        curr_pos_at_start = curr_pos;
         price_today = stod(data[i][1]);
-        arr[(i - 1) % n] = price_today - stod(data[i - 1][1]);
-        // print_arr(arr, n);
-        ER = calc_ER_dmapp(arr, n);
-        SF_today = calc_SF_dmapp(SF_today, ER, c1, c2, i - n);
-        AMA_today = calc_AMA_dmapp(AMA_today, price_today, SF_today, i - n);
 
         double percent_diff = percent_difference_dmapp(AMA_today, price_today);
         vector<string> CashFlow_row;
         string date = date_formatting_dmapp(data[i][0]);
+
         if (percent_diff > p)
         {
             if (curr_pos < x)
@@ -154,23 +152,22 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
                 else if (curr_pos == 0)
                 {
                     arr2[first_buy_sell] = i;
-                    arr2[this_buy_sell] = i;
+                    // arr2[this_buy_sell] = i; since first_buy_sell = this_buy_sell, this commented operation
+                    // was redundant, actually first_buy_sell = 0, but I have used directly first_buy_sell for better clarity
                     this_buy_sell = (this_buy_sell + 1) % x;
                 }
                 else
                 {
                     // we are buying the stock
-                    // if max_hold_days says we neeed to sell the stock then in that case we do not write anything
+                    // if max_hold_days says we need to sell the stock then in that case we do not write anything
                     // to order stats file but just update the date i.e remov the first buy and put it into today buy
                     if ((i - arr2[first_buy_sell]) >= max_hold_days)
                     {
-                        // cout << "const from buy case  " + date << endl;
                         arr2[first_buy_sell] = -1;
                         first_buy_sell = (first_buy_sell + 1) % x;
                         arr2[this_buy_sell] = i;
                         this_buy_sell = (this_buy_sell + 1) % x;
-                        check_buy_sell = true; // it check if we have sell and buy on the same day or not
-                                               // if we had done that then we will not update
+                        curr_pos--;
                     }
                     else
                     {
@@ -178,36 +175,18 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
                         this_buy_sell = (this_buy_sell + 1) % x;
                     }
                 }
-                if (check_buy_sell == false)
-                {
-                    vector<string> order_stats_row;
-                    cumulative_cashflow -= price_today;
-                    order_stats_row.push_back(date);
-                    order_stats_row.push_back("BUY");
-                    order_stats_row.push_back("1");
-                    order_stats_row.push_back(data[i][1]);
-                    order_stats.push_back(order_stats_row);
-                    curr_pos++;
-                }
-                check_buy_sell = false;
+
+                curr_pos++;
             }
 
             else if (curr_pos == x)
             {
                 // need to check if we have to close any of the position
-                // since we are not buyig any stock we can sell the stock if required
+                // since we are not buying any stock we can sell the stock if required
                 if ((i - arr2[first_buy_sell]) >= max_hold_days)
                 {
-                    // cout << "const from buy case 2 " + date << endl;
                     arr2[first_buy_sell] == -1;
                     first_buy_sell = (first_buy_sell + 1) % x;
-                    vector<string> order_stats_row;
-                    cumulative_cashflow += price_today;
-                    order_stats_row.push_back(date);
-                    order_stats_row.push_back("SELL");
-                    order_stats_row.push_back("1");
-                    order_stats_row.push_back(data[i][1]);
-                    order_stats.push_back(order_stats_row);
                     curr_pos--;
                 }
             }
@@ -233,23 +212,21 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
                 else if (curr_pos == 0)
                 {
                     arr2[first_buy_sell] = i;
-                    arr2[this_buy_sell] = i;
+                    // same reasoning as the buying stock case
                     this_buy_sell = (this_buy_sell + 1) % x;
                 }
                 else
                 {
                     // we are selling the stock
                     // if max_hold_days says we need to buy the stock then in that case we do not write anything
-                    // to order stats file but just update the date i.e remove the first buy and put it into today buy
+                    // to order stats file but just update the date(of first buy and this buy) i.e remove the first buy and put it into today buy
                     if ((i - arr2[first_buy_sell]) >= max_hold_days)
                     {
-                        // cout << "const from sell case  " + date << endl;
                         arr2[first_buy_sell] = -1;
                         first_buy_sell = (first_buy_sell + 1) % x;
                         arr2[this_buy_sell] = i;
                         this_buy_sell = (this_buy_sell + 1) % x;
-                        check_buy_sell = true; // it check if we have buy and sell on the same day or not
-                                               // if we had done that then we will not update order stats
+                        curr_pos++;
                     }
                     else
                     {
@@ -257,36 +234,17 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
                         this_buy_sell = (this_buy_sell + 1) % x;
                     }
                 }
-                if (check_buy_sell == false)
-                {
-                    vector<string> order_stats_row;
-                    cumulative_cashflow += price_today;
-                    order_stats_row.push_back(date);
-                    order_stats_row.push_back("SELL");
-                    order_stats_row.push_back("1");
-                    order_stats_row.push_back(data[i][1]);
-                    order_stats.push_back(order_stats_row);
-                    curr_pos--;
-                }
-                check_buy_sell = false;
+                curr_pos--;
             }
 
             else if (curr_pos == -x)
             {
                 // need to check if we have to close any of the position
-                // since we are not buyig any stock we can sell the stock if required
+                // since we are not selling any stock we can buy the stock if required
                 if ((i - arr2[first_buy_sell]) >= max_hold_days)
                 {
-                    // cout << "const from sell case 2  " + date << endl;
                     arr2[first_buy_sell] == -1;
                     first_buy_sell = (first_buy_sell + 1) % x;
-                    vector<string> order_stats_row;
-                    cumulative_cashflow -= price_today;
-                    order_stats_row.push_back(date);
-                    order_stats_row.push_back("BUY");
-                    order_stats_row.push_back("1");
-                    order_stats_row.push_back(data[i][1]);
-                    order_stats.push_back(order_stats_row);
                     curr_pos++;
                 }
             }
@@ -302,27 +260,39 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
                     CashFlow_row.push_back(to_string(arr2[first_buy_sell]));
                     arr2[first_buy_sell] == -1;
                     first_buy_sell = (first_buy_sell + 1) % x; // modifying the first buy sell
-                    vector<string> order_stats_row;
-                    order_stats_row.push_back(date);
+
                     if (curr_pos < 0)
                     {
-                        order_stats_row.push_back("BUY");
-                        cumulative_cashflow -= price_today;
                         curr_pos++;
                     }
                     else
                     {
-                        order_stats_row.push_back("SELL");
-                        cumulative_cashflow += price_today;
                         curr_pos--;
                     }
-                    order_stats_row.push_back("1");
-                    order_stats_row.push_back(data[i][1]);
-
-                    order_stats.push_back(order_stats_row);
                 }
             }
         }
+
+        int change = curr_pos - curr_pos_at_start;
+
+        if (change != 0)
+        {
+            vector<string> order_stats_row;
+            cumulative_cashflow -= change * price_today;
+            order_stats_row.push_back(date);
+            if (change < 0)
+            {
+                order_stats_row.push_back("BUY");
+            }
+            else
+            {
+                order_stats_row.push_back("BUY");
+            }
+            order_stats_row.push_back("1");
+            order_stats_row.push_back(data[i][1]);
+            order_stats.push_back(order_stats_row);
+        }
+
         CashFlow_row.push_back(date);
         CashFlow_row.push_back(to_string(cumulative_cashflow));
         CashFlow_row.push_back(to_string(AMA_today));
@@ -331,9 +301,16 @@ double dmapp_strategy(const vector<vector<string>> &data, vector<vector<string>>
         CashFlow_row.push_back(to_string(ER));
         CashFlow_row.push_back(to_string(percent_diff));
         CashFlow.push_back(CashFlow_row);
-        // after the current day buy/sell is done then we will do update in the values to be used for next day
 
-        // cout << date + " " << i << " " << curr_pos << endl;
+        // cout << date + " " << first_buy_sell << "   " << date << "   " << arr2[first_buy_sell] << " i= " << to_string(i) <<endl;
+        if (i < data_length - 1)
+        {
+            arr[i % n] = stod(data[i + 1][1]) - price_today;
+            price_today = stod(data[i + 1][1]);
+            ER = calc_ER_dmapp(arr, n);
+            SF_today = calc_SF_dmapp(SF_today, ER, c1, c2, 1);
+            AMA_today = calc_AMA_dmapp(AMA_today, price_today, SF_today, 1);
+        }
     }
 
     cumulative_cashflow += curr_pos * stod(data[data_length - 1][1]);
